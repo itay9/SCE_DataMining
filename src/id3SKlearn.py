a7328b72384bf8c17e55ac0a72f11d158d645ba2
chenaz1 <57365377+chenaz1@users.noreply.github.com>
2020-07-28 19:13:59 +0300

    adding functions

---
+++ src/id3SKlearn.py
@@ -0,0 +1,21 @@
+from sklearn.tree import DecisionTreeClassifier
+from functions import Discretize,fit_transforms
+
+numOfBins=3
+def ID3SKlearn_algorithm(train,test,structure):
+    train = Discretize(numOfBins, train, structure)
+    test = Discretize(numOfBins, test, structure)
+
+    train=fit_transforms(train)
+    train_target= train['class']
+    train_feature=train.drop('class', axis='columns')
+
+    test=fit_transforms(test)
+    test_target= test['class']
+    test_feature=test.drop('class', axis='columns')
+
+    tree=DecisionTreeClassifier(criterion='entropy',max_depth=100).fit(train_feature,train_target)
+
+    prediction = tree.predict(test_feature)
+
+    print("ID3SKlearn_algorithm accuracy is: ",tree.score(test_feature,test_target)*100,"%")
