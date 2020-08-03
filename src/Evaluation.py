e1d8e45365260be223713f6b55db1dff54ac906c
itay9 <itaydali@gmail.com>
2020-08-03 21:11:04 +0300

    .

--- src/Evaluation.py
+++ src/Evaluation.py
@@ -1,8 +1,7 @@
 import pandas as pd
 
 
-
-def calc(tp,tn,fp,fn):
+def calc(tp, tn, fp, fn):
     """
 
     :param tp: match_yes
@@ -11,30 +10,41 @@ def calc(tp,tn,fp,fn):
     :param fn: fail_no
     :return:
     """
-    accuracy = (tp+tn)/(tp+tn+fn+fp)
-    recall = tp/(tp+fn)
-    precision = tp/(tp+fp)
-    fMeasure = 2*precision*recall/(precision+recall)
+    accuracy = (tp + tn) / (tp + tn + fn + fp)
+    recall = tp / (tp + fn)
+    precision = tp / (tp + fp)
+    fMeasure = 2 * precision * recall / (precision + recall)
     print("Evaluation Indicators:")
-    print("Accuracy: ",accuracy)
-    print("Recall: ",recall)
-    print("Precision: ",precision)
-    print("fMeasure: ",fMeasure)
+    print("Accuracy: ", accuracy)
+    print("Recall: ", recall)
+    print("Precision: ", precision)
+    print("fMeasure: ", fMeasure)
     print()
 
-def fixStr(val,name):
-    tmp = name.upper()+" = "+str(val)
-    return tmp
-def buildMatrix(tp,tn,fp,fn):
 
-    data = {'Negative':[fixStr(tn,'tn'),fixStr(fn,'fn')],'Positive':[fixStr(fp,'fp'),fixStr(tp,'tp')]}
+def fixStr(val, name):
+    tmp = name.upper() + " = " + str(val)
+    return tmp
+
+
+def buildMatrix(tp, tn, fp, fn):
+    """
+    building matrix
+    :param tp:
+    :param tn:
+    :param fp:
+    :param fn:
+    :return:
+    """
+
+    data = {'Negative': [fixStr(tn, 'tn'), fixStr(fn, 'fn')], 'Positive': [fixStr(fp, 'fp'), fixStr(tp, 'tp')]}
     matrix = pd.DataFrame(data)
     print("Confusion matrix:")
     print()
     print(matrix)
     print()
 
-def Eval(tp,tn,fp,fn):
-    calc(tp,tn,fp,fn)
-    buildMatrix(tp,tn,fp,fn)
 
+def Eval(tp, tn, fp, fn):
+    calc(tp, tn, fp, fn)
+    buildMatrix(tp, tn, fp, fn)
